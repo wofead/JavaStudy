f1b49a439c5dc747219ba66d2b168240a69ab56a
wofead <614667387@qq.com>
2019-01-10 21:53:14 +0800

    test for git

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         QueueTest test = new QueueTest();
         test.test();
+        System.out.println("test for git");
     }
 
 }
