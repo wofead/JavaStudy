8e0510d61a8aff0f63b58de63e53d2e262ae4218
wofead <h614667387@qq.com>
2019-03-13 23:46:29 +0800

    冒泡排序和斐波那契数列

---
+++ src/Algorithm/FibonacciAlgorithm/FabonacciTest.java
@@ -0,0 +1,14 @@
+package Algorithm.FibonacciAlgorithm;
+
+/**
+ * @Author: jow
+ * @Date: 2019/3/13 23:42
+ * @Description
+ * @Version 1.0
+ */
+public class FabonacciTest {
+    public void fabo(){
+        Fibonacci fibonacci = new Fibonacci();
+        System.out.println(fibonacci.fibonacciAl(6));
+    }
+}
