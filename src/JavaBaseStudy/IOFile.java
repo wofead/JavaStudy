8a8109cf325aa3c91ad63ab77e52fb42e62ad1a0
wofead <614667387@qq.com>
2019-03-20 21:59:38 +0800

    字节输入输出 字符输入输出

---
+++ src/JavaBaseStudy/IOFile.java
@@ -0,0 +1,84 @@
+package JavaBaseStudy;
+
+import java.io.*;
+
+public class IOFile {
+
+//    字节输入输出流
+    public void ioStudyByte(){
+        File file = new File("./word.text");
+        try {
+            FileOutputStream outputStream = new FileOutputStream(file);
+            byte buy[] = "i have a horse but don't rose for ever.".getBytes();
+            outputStream.write(buy);
+            outputStream.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            byte [] byt = new byte[1024];
+            int len = fileInputStream.read(byt);
+            System.out.println("文件中的信息是" + new String(byt,0,len));
+            fileInputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //    字符输入输出流
+    public void ioStudyString(){
+        File file = new File("./word.text");
+        try {
+            FileWriter outputStream = new FileWriter(file);
+            String buy = "i have a tiger but don't rose for ever.";
+            outputStream.write(buy);
+            outputStream.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            FileReader fileInputStream = new FileReader(file);
+            char [] byt = new char[1024];
+            int len = fileInputStream.read(byt);
+            System.out.println("文件中的信息是" + new String(byt,0,len));
+            fileInputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+//    带缓冲区的字符输入输出流
+    public void ioStringBuffer(){
+        File file = new File("./word.text");
+        String[] strings = {"i'm jow","wofead","hello"};
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            for (int i = 0; i < strings.length; i++) {
+                bufferedWriter.write(strings[i]);
+                bufferedWriter.newLine();
+            }
+            bufferedWriter.close();
+            fileWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String s = null;
+            while((s = bufferedReader.readLine()) != null){
+                System.out.println(s);
+            }
+            bufferedReader.close();
+            fileReader.close();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
